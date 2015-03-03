d79525ce43bf799eadd400d7e32aa11fd3463802
Gregory Borysiak <gregory.borysiak@infor-bs.com>
2015-03-03 16:15:36 +0100

    First release

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerminalTools;
+
+namespace GetRdpSession
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if( args.Length == 0 )
+            {
+                Console.Write("Error no arguments. Specify server name");
+                return 1;
+            }
+
+            try
+            {
+                String serverName = args[0];
+                List<TerminalSessionData> listSessions = TerminalTools.TermServicesManager.ListSessions(serverName);
+                foreach (TerminalSessionData session in listSessions)
+                {
+                    TerminalSessionInfo info = TerminalTools.TermServicesManager.GetSessionInfo(serverName, session.SessionId);
+                    if (info.UserName != null && ( info.UserName != null && info.UserName.Trim() != "" ) )
+                    {
+                        Console.WriteLine(session.SessionId + " " + info.UserName + " " + session.ConnectionState);
+                    }
+                }
+                return 0;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 2;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GetRdpSession")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GetRdpSession")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b103ca4f-123e-4c30-a43a-a7b46c2da719")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
